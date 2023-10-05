44483d8e5116556b2cd079bb0ddc7b5c52689c9f
Mochi McCoy <hi@mochibun.me>
2023-10-05 19:28:51 +0200

    Added random quote to 404 page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,21 @@
-  import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
+  import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar, Switch} from "@nextui-org/react";
   import {AcmeLogo} from "../assets/AcmeLogo.jsx";
   import {SearchIcon} from "../assets/SearchIcon.jsx";
   import { useState, useEffect } from "react";
+import { SunIcon } from "../assets/SunIcon.jsx";
+import { MoonIcon } from "../assets/MoonIcon.jsx";
 
   const Nav = () => {
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [signedIn, setSignedIn] = useState(false);
+    const [isSelected, setIsSelected] = useState(true);
+
+    useEffect (() => {}, []);
+
+    const handleClick = () => {
+      setSignedIn(!signedIn);
+    }
 
     const menuItems = [
       "Profile",
@@ -31,22 +41,22 @@
 
           <NavbarBrand className="mr-4">
             <AcmeLogo className="place-items-center" />
-            <p className="hidden sm:block font-bold text-inherit">Viny Countdown</p>
+            <p className="hidden sm:block font-bold text-inherit">VINYL COUNTDOWN</p>
           </NavbarBrand>
           <NavbarContent className="hidden sm:flex gap-3">
-            <NavbarItem>
-              <Link color="foreground" href="#">
-                Features
-              </Link>
-            </NavbarItem>
             <NavbarItem isActive>
               <Link href="#" aria-current="page" color="secondary">
-                Customers
+                Home
               </Link>
             </NavbarItem>
             <NavbarItem>
               <Link color="foreground" href="#">
-                Integrations
+                New
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Browse
               </Link>
             </NavbarItem>
           </NavbarContent>
@@ -65,6 +75,14 @@
             startContent={<SearchIcon size={18} />}
             type="search"
           />
+          <Switch
+            isSelected={isSelected}
+            onValueChange={setIsSelected}
+            size="lg"
+            color="warning"
+            startContent={<SunIcon />}
+            endContent={<MoonIcon />}
+            ></Switch>
           <Dropdown placement="bottom-end">
             <DropdownTrigger>
               <Avatar
@@ -72,24 +90,26 @@
                 as="button"
                 className="transition-transform"
                 color="secondary"
-                name="Jason Hughes"
+                name="John Madden"
                 size="sm"
                 src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
               />
             </DropdownTrigger>
-            <DropdownMenu aria-label="Profile Actions" variant="flat"> {/* Avatar menu items */}
+            <DropdownMenu aria-label="Profile Actions" variant="flat" disabledKeys={["forums", "settings", "help_and_feedback"]}> {/* Avatar menu items */}
               <DropdownItem key="profile" className="h-14 gap-2">
-                <p className="font-semibold">Signed in as</p>
-                <p className="font-semibold">john.madden@nfl.gov</p>
+                {signedIn ? <>
+                    <p className="font-semibold">Signed in as</p>
+                    <p className="font-normal">john.madden@nfl.gov</p> {/* //!Placeholder */}
+                  </>
+                  : "Not signed in"}
               </DropdownItem>
-              <DropdownItem key="settings">My Settings</DropdownItem>
-              <DropdownItem key="team_settings">Team Settings</DropdownItem>
-              <DropdownItem key="analytics">Analytics</DropdownItem>
-              <DropdownItem key="system">System</DropdownItem>
-              <DropdownItem key="configurations">Configurations</DropdownItem>
+              <DropdownItem key="orders">Orders</DropdownItem>
+              <DropdownItem key="lists">Lists</DropdownItem>
+              <DropdownItem key="forums">Forums</DropdownItem>
+              <DropdownItem key="settings">Settings</DropdownItem>
               <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-              <DropdownItem key="logout" color="danger">
-                Sign Out
+              <DropdownItem key={signedIn ? "logout" : "login"} color={signedIn ? "danger" : "warning"} onClick={handleClick}>
+                {signedIn ? "Sign Out" : "Sign In"}
               </DropdownItem>
             </DropdownMenu>
           </Dropdown>
