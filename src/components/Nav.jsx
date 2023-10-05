77aa6970366b01c45009f15515a0821eec2b0581
Mochi McCoy <hi@mochibun.me>
2023-10-05 17:20:00 +0200

    Added navbar with responsiveness, WIP

---
+++ src/components/Nav.jsx
@@ -0,0 +1,119 @@
+  import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
+  import {AcmeLogo} from "../assets/AcmeLogo.jsx";
+  import {SearchIcon} from "../assets/SearchIcon.jsx";
+  import { useState, useEffect } from "react";
+
+  const Nav = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const menuItems = [
+      "Profile",
+      "Dashboard",
+      "Activity",
+      "Analytics",
+      "System",
+      "Deployments",
+      "My Settings",
+      "Team Settings",
+      "Help & Feedback",
+      "Log Out",
+    ];
+
+    return (
+      <Navbar shouldHideOnScroll onMenuOpenChange={setIsMenuOpen}>
+        <NavbarContent justify="start">
+
+        <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
+          />
+
+          <NavbarBrand className="mr-4">
+            <AcmeLogo className="place-items-center" />
+            <p className="hidden sm:block font-bold text-inherit">Viny Countdown</p>
+          </NavbarBrand>
+          <NavbarContent className="hidden sm:flex gap-3">
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Features
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive>
+              <Link href="#" aria-current="page" color="secondary">
+                Customers
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Integrations
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+        </NavbarContent>
+
+        <NavbarContent as="div" className="items-center" justify="end">
+          <Input
+            classNames={{
+              base: "max-w-full sm:max-w-[10rem] h-10",
+              mainWrapper: "h-full",
+              input: "text-small",
+              inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            }}
+            placeholder="Type to search..."
+            size="sm"
+            startContent={<SearchIcon size={18} />}
+            type="search"
+          />
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="secondary"
+                name="Jason Hughes"
+                size="sm"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat"> {/* Avatar menu items */}
+              <DropdownItem key="profile" className="h-14 gap-2">
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">john.madden@nfl.gov</p>
+              </DropdownItem>
+              <DropdownItem key="settings">My Settings</DropdownItem>
+              <DropdownItem key="team_settings">Team Settings</DropdownItem>
+              <DropdownItem key="analytics">Analytics</DropdownItem>
+              <DropdownItem key="system">System</DropdownItem>
+              <DropdownItem key="configurations">Configurations</DropdownItem>
+              <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+              <DropdownItem key="logout" color="danger">
+                Sign Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarContent>
+
+        <NavbarMenu>
+          {menuItems.map((item, index) => ( //Hamburger menu items
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                color={
+                  index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+                }
+                className="w-full"
+                href="#"
+                size="lg"
+              >
+                {item}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+
+      </Navbar>
+    );
+  }
+
+export default Nav;
