f5ebc4ab8cd29aba8efbb1bd36a239d3316dc428
Mochi McCoy <hi@mochibun.me>
2023-10-07 00:29:33 +0200

    Minor changes

--- src/pages/AlbumPage.jsx
+++ src/pages/AlbumPage.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { createClient } from "contentful";
-import { Card, CardBody, CardFooter, Image, Divider, Textarea, Button } from "@nextui-org/react";
+import { Card, CardBody, Image, Divider, Textarea, Button } from "@nextui-org/react";
 import AlbumCarousel from "../components/AlbumCarousel";
 import { Spotify } from "react-spotify-embed";
 
@@ -37,15 +37,15 @@ export default function AlbumPage() {
                         shadow="sm"
                         isPressable
                         onPress={() => console.log("item pressed")}
-                        className="mb-8"
+                        className="mb-8 h-full"
                         >
-                        <CardBody className="overflow-visible p-0">
+                        <CardBody className=" p-0">
                             <Image
                                 shadow="sm"
                                 radius="lg"
                                 width="100%"
                                 alt={singleAlbum.title}
-                                className="sticky sm:max-h-[250px] md:max-h-[300px] lg:max-h-[450px] xl:max-h-[600px]"
+                                className="sm:max-h-[250px] md:max-h-[300px] lg:max-h-[450px] xl:max-h-[600px]"
                                 src={singleAlbum.imgUrl}
                                 />
                         </CardBody>
@@ -54,8 +54,8 @@ export default function AlbumPage() {
                             <p className="text-default-500">{singleAlbum.price}</p>
                         </CardFooter> */}
                     </Card>
-                    <div className="sm:ml-5 md:ml-8 lg:ml-10 sm:mt-12 w-max">
-                        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">{singleAlbum.title}</h1>
+                    <div className="sm:ml-5 md:ml-8 lg:ml-10 sm:mt-12 w-1/2">
+                        <span className="text-3xl md:text-4xl lg:text-5xl font-bold">{singleAlbum.title}</span>
                         <h2 className="text-3xl sm:text-xl md:text-2xl lg:text-3xl font-semibold mt-2">{singleAlbum.artist}</h2>
                         <Divider className="my-4 sm:my-2" />
                         <h1 className="text-4xl lg:text-6xl font-bold">${singleAlbum.price}</h1>
@@ -63,7 +63,7 @@ export default function AlbumPage() {
                         <h2 className="lg:text-3xl text-xl">Sleeve: <b className="font-semibold">{singleAlbum.sleeve}</b></h2>
                         <h2 className="lg:text-3xl text-xl">Media: <b className="font-semibold">{singleAlbum.media}</b></h2>
                         
-                        <div className="w-full max-w-full sm:max-w-xs md:max-w-md lg:max-w-lg">
+                        <div className="w-full max-w-full sm:max-w-xs md:max-w-md">
                             <Textarea
                                 isReadOnly
                                 label="Additional notes"
