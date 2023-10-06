dca6fc4c6eee249bdf236b47e64e8d8e2d82fbed
gjlander <garrett.jlander@gmail.com>
2023-10-06 17:32:23 +0200

    added a styles folder, made a first run of a functional carousel

--- src/components/AlbumCarousel.jsx
+++ src/components/AlbumCarousel.jsx
@@ -1,7 +1,81 @@
-export default function AlbumCarousel() {
+import { useState, useEffect } from "react";
+import Slider from "react-slick";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { getNewArrivals } from "../lib/contentfulClient";
+import AlbumCard from "./AlbumCard";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function Carousel() {
+    const [sliderRef, setSliderRef] = useState(null);
+    const [albumRecs, setAlbumRecs] = useState();
+    useEffect(() => {
+        getNewArrivals()
+            .then((albumData) => setAlbumRecs(albumData))
+            .catch((error) => console.error(error));
+    }, []);
+
+    const sliderSettings = {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    };
     return (
-        <>
-            <div>A carousel</div>
-        </>
+        <div className="w-11/12 p-4 mx-auto">
+            <h4 className="text-2xl font-bold">You may also like...</h4>
+            <div className="w-full flex justify-between relative z-10 top-48 right-4">
+                <button onClick={sliderRef?.slickPrev}>
+                    <FaChevronLeft />
+                </button>
+                <button onClick={sliderRef?.slickNext}>
+                    <FaChevronRight />
+                </button>
+            </div>
+            <Slider className="px-5" ref={setSliderRef} {...sliderSettings}>
+                {albumRecs &&
+                    albumRecs.map((rec) => (
+                        <AlbumCard
+                            key={crypto.randomUUID()}
+                            {...rec.fields}
+                            id={rec.sys.id}
+                        />
+                        // <div key={index} className="card">
+                        //     <img
+                        //         src={card.fields.imgUrl}
+                        //         alt={card.fields.title}
+                        //         className="card-image"
+                        //     />
+                        //     <div className="text-info">
+                        //         <div className="card-header">
+                        //             <h2>{card.fields.title}</h2>
+                        //             <span>{card.fields.price}</span>
+                        //         </div>
+                        //         <p>{card.description}</p>
+                        //         <ul>
+                        //         {card.features.map((feature, index) => (
+                        //             <li key={index}>{feature}</li>
+                        //         ))}
+                        //     </ul>
+                        //     </div>
+                        //     <button>Buy Now</button>
+                        // </div>
+                    ))}
+            </Slider>
+        </div>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
 import App from "./App";
-import "./index.css";
+import "./styles/index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
