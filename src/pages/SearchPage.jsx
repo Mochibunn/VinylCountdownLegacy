8bac6327685673c9ce9c7725ef697760b1c057f4
gjlander <garrett.jlander@gmail.com>
2023-10-07 13:01:31 +0200

    adding styling to searchpage, currently broken

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -7,10 +7,42 @@ import {
     Configure,
 } from "react-instantsearch";
 
+import SearchCard from "../components/SearchCard";
+
 const searchClient = algoliasearch(
     "P6CCZN45ER",
     "cf814256b4cf855690036f02a8e267d0"
 );
+
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Divider,
+    Link,
+    Image,
+} from "@nextui-org/react";
+
+export function SearchCard() {
+    return (
+        <Card className="max-w-[400px]">
+            <CardBody>
+                <Image
+                    alt="nextui logo"
+                    height={40}
+                    radius="sm"
+                    src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+                    width={40}
+                />
+                <div className="flex flex-col">
+                    <p className="text-md">NextUI</p>
+                    <p className="text-small text-default-500">nextui.org</p>
+                </div>
+            </CardBody>
+        </Card>
+    );
+}
 
 function Hit({ hit }) {
     return (
