37e3e44423e6d88243c3a087fee34f0722a4e01e
gjlander <garrett.jlander@gmail.com>
2023-10-07 12:47:14 +0200

    zero styling, but search function works!

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,36 @@
+import algoliasearch from "algoliasearch/lite";
+import {
+    InstantSearch,
+    SearchBox,
+    Hits,
+    Pagination,
+    Configure,
+} from "react-instantsearch";
+
+const searchClient = algoliasearch(
+    "P6CCZN45ER",
+    "cf814256b4cf855690036f02a8e267d0"
+);
+
+function Hit({ hit }) {
+    return (
+        <article>
+            <img src={hit.fields.imgUrl["en-US"]} />
+            {/* <p>{hit.categories[0]}</p> */}
+            <h1>{hit.fields.title["en-US"]}</h1>
+            <p>${hit.fields.price["en-US"]}</p>
+        </article>
+    );
+}
 export default function SearchPage() {
     return (
         <>
-            <h1>Here is where our search page is gonna go!</h1>
+            <InstantSearch searchClient={searchClient} indexName="albums">
+                <Configure hitsPerPage={10} />
+                <SearchBox />
+                <Hits hitComponent={Hit} />
+                <Pagination />
+            </InstantSearch>
         </>
     );
 }
